7abf57ded6c6d15e721905195278517546d050e3
Pete Barnard <phbarna@tiscali.co.uk>
2023-05-18 20:27:21 +0100

    used chatGPT to help me conditional render the async if no server

--- src/pages/AsyncTest/index.jsx
+++ src/pages/AsyncTest/index.jsx
@@ -1,19 +1,16 @@
 import { useState, useEffect } from "react";
 
 export default function AsyncTest() {
-
-  //example 
+  //example
   async function getData() {
     try {
       const response = await fetch("http://192.168.1.91:1337/data");
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error(error);
-
+      console.log("error");
     }
   }
-
 
   const [data, setData] = useState([]);
 
@@ -21,19 +18,29 @@ export default function AsyncTest() {
     const fetchData = async () => {
       const result = await getData();
       setData(result);
-    }
+    };
     fetchData();
   }, []);
 
   return (
     <>
       <div className="intro">
-        Async example.  Relies on having a separate server at 192.168.1.91:1337/data
+        Async example. Relies on having a separate server at
+        192.168.1.91:1337/data
       </div>
       <div>
-        {data?.map(item => (
-          <p key={item.id}>{item.name}  age:{item.age } gender:{item.gender} from {item.location}</p>
-        ))}
+        {data ? (
+          <ol>
+            {data.map((item) => (
+              <p key={item.id}>
+                {item.name} age:{item.age} gender:{item.gender} from{" "}
+                {item.location}
+              </p>
+            ))}
+          </ol>
+        ) : (
+          <div className="error">Cannot connect to server !</div>
+        )}
       </div>
     </>
   );
